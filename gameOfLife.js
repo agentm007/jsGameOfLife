3fb31b07a003e82a91e52536d59a7af703851b95
root <dexmount@hotmail.com>
2017-10-17 20:24:00 -0500

    changed the code into index for the moment. Created the basic grid

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,12 +1,15 @@
 
-var width, height;
+var width, height, boxwidth, boxheight;
 width = 40;
 height = 40;
+boxheight = 5;
+boxwidth = 5;
 
+function createBoard(){
 
-
-for(var i=0; i<width; i++){
-	for(var j=0; j<height; j++){
-		
+	for(var i=0; i<width; i++){
+		for(var j=0; j<height; j++){
+			document.write(i,j);
+		}
 	}
 }
